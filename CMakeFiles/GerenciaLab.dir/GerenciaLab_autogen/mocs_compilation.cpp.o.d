e51c02b463e7ee5fdecaabc20c2d8b2777015bc6
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2025-08-19 19:26:19 -0300

    Limpar repositório: remover arquivos CMake e atualizar .gitignore

--- CMakeFiles/GerenciaLab.dir/GerenciaLab_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/GerenciaLab.dir/GerenciaLab_autogen/mocs_compilation.cpp.o: \
-  /Users/luizhenrique/Desktop/gerencialab/GerenciaLab_autogen/mocs_compilation.cpp
--- src/lab-fila/CMakeFiles/lab-fila_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lab-fila_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
